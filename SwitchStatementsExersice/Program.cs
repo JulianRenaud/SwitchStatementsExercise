daa4648be798b63dd5652382ee8f4cd51859853f
JulianRenaud <midway6220@gmail.com>
2022-09-15 09:18:41 -0500

    First Commit

---
+++ SwitchStatementsExersice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mail;
+
+Console.WriteLine("What is your favorite school subject?");
+
+var subject = Console.ReadLine();
+switch (subject)
+{
+    case "Math":
+        Console.WriteLine("Math can be challenging sometimes!");
+        break;
+    case "Science":
+        Console.WriteLine("Science is a tough subject!");
+        break;
+    case "English":
+            Console.WriteLine("English is very interesting!");
+        break;
+    default:
+            Console.WriteLine($"{subject} is very fun and can be a challenge sometimes!");
+        break;
+}
